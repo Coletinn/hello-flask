73e16bfb8284eb48cd6c8f1c5956a21d93276650
Coletinn <gustavocoleto@hotmail.com>
2024-12-05 22:03:41 -0300

    initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+import uuid
+
+app = Flask(__name__)
+
+users = {}
+
+@app.route('/', methods=['GET'])
+def hello():
+    return '<h1>Hello world</h1>'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
